066571a2c5187f548e54b158b1eb2d9c45618d64
maurotoro94 <maurotoro11@gmail.com>
2021-05-11 23:09:47 -0500

    Entrega del sprint1 almacen

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,48 +6,47 @@ var totalpagar= 0;
 
 
 function Calcular(){
-    cantidad = document.getElementById('idCantidad').value;
+    
+    cantidad = document.getElementById('cantidad').value;
     console.log(cantidad);
 
-   valorcompra= cantidad*valor ;
+   valorcompra= cantidad* 820000;
+   console.log(valorcompra);
 
-   if (valorcompra = 820000){
-    totalpagar = valorcompra ;
-        document.getElementById('salario').value = salario;
-        document.getElementById('descuento') .value = 0;
-        document.getElementById('salarioapagar').value = salario;
-        console.log(salario);
+   if (valorcompra == 820000 || valorcompra > 9840000){
+    totalpagar = valorcompra;
+    
+        document.getElementById("valordelacompra").value = valorcompra;
+        document.getElementById("valordedescuento").value = 0;
+        document.getElementById("totalaPagar").value = totalpagar;
+        console.log(totalpagar);
     }
 
  else if(valorcompra >=1640000 && valorcompra <=3280000){
         totalpagar = valorcompra - (valorcompra * 0.15);
-            document.getElementById('salario').value = salario;
-            document.getElementById('descuento').value = (valorcompra * 0.15);
-            document.getElementById('salarioapagar').value = salario;
-            console.log(salario);
+        document.getElementById("valordelacompra").value = valorcompra;
+            document.getElementById("valordedescuento").value = (valorcompra * 0.15);
+            document.getElementById("totalaPagar").value = totalpagar;
+            console.log(totalpagar);
         }
 
  else if(valorcompra >3280000 && valorcompra <=6560000){
             totalpagar = valorcompra - (valorcompra * 0.25);
-            document.getElementById('salario').value = salario;
-            document.getElementById('salarioextra').value = 0;
-            document.getElementById('salarioapagar').value = salario;
-            //console.log(salario);
+            document.getElementById("valordelacompra").value = valorcompra;
+            document.getElementById("valordedescuento").value = (valorcompra * 0.25);
+            document.getElementById("totalaPagar").value = totalpagar;
+            console.log(totalpagar);
            }
 
- else if(valorcompra >6560000 && valorcompra <=9840000){
-            totalpagar = valorcompra - (valorcompra * 0.25);
-            document.getElementById('salario').value = salario;
-            document.getElementById('salarioextra').value = 0;
-            document.getElementById('salarioapagar').value = salario;
-            //console.log(salario);
+ else if (valorcompra >6560000 && valorcompra <=9840000){
+            totalpagar = valorcompra - (valorcompra * 0.35);
+            document.getElementById("valordelacompra").value = valorcompra;
+            document.getElementById("valordedescuento").value = (valorcompra * 0.35);
+            document.getElementById("totalaPagar").value = totalpagar;
+            console.log(totalpagar);
            }
+ else{
 
-else(valorcompra > 9840000){
-    totalpagar = valorcompra;
-    document.getElementById('salario').value = salario;
-    document.getElementById('salarioextra').value = 0;
-    document.getElementById('salarioapagar').value = salario;
-    //console.log(salario);
-   }
- 
+ }
+
+}
